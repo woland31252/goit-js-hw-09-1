8079ef7c84a0287161a24e6de42798cb51870a26
woland31252 <110925314+woland31252@users.noreply.github.com>
2024-02-09 20:34:30 +0000

    Deploying to gh-pages from @ woland31252/goit-js-hw-09-1@2c65e156f3043e785d8648b9458f16ae26cf8aa4 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,5 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+import"./assets/common-3d5c7ba9.js";const a=document.querySelector(".gallery"),r=images.map(l=>`<li class="gallery-item">
+  <a class="gallery-link" href="${l.original}">
+    <img class="gallery-image" src="${l.preview}"
+      data-source="${l.original}" alt="${l.description}"/></a></li>`).join("");a.innerHTML=r;
 //# sourceMappingURL=commonHelpers.js.map
