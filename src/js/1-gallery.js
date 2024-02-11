78e2556110f0a323ceb52b1f11fe9d3148d81525
Volodymyr <110925314+woland31252@users.noreply.github.com>
2024-02-11 18:56:19 +0200

    gallery

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,10 +1,18 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { images } from './gallery-items';
+
 const gallery = document.querySelector('.gallery');
 const galleryItem = images
   .map(
     elem => `<li class="gallery-item">
   <a class="gallery-link" href="${elem.original}">
-    <img class="gallery-image" src="${elem.preview}"
-      data-source="${elem.original}" alt="${elem.description}"/></a></li>`
+    <img class="gallery-image" src="${elem.preview}" 
+    alt="${elem.description}"/></a></li>`
   )
   .join('');
 gallery.innerHTML = galleryItem;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
